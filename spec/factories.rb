ededb116fac1468578982f4ab755a6a6ae88fffa
Roviry <ross@roviry.com.au>
2013-05-26 17:12:11 +1000

    Chapter 7

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name "Ross Bamford"
+        email "ross@castingspider.com"
+        password "pilot99"
+        password_confirmation "pilot99"
+    end
+end
